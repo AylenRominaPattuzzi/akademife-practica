9788314a324796ba299950dd1145a4600aabfa53
AylenRominaPattuzzi <aylenrominapattuzzi@gmail.com>
2025-05-23 14:25:00 -0300

    ..

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(checkAuthThunk()); // importante
+    dispatch(checkAuthThunk());
   }, [dispatch]);
 
   return (
